c812bfe772e811959727ca98022ff1654802f9fc
Nattawut Boonnoon <leonardo_90s@hotmail.com>
2025-12-27 17:19:57 +0700

    Delete Pandas directory

--- Pandas/001_Intro_Pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-print(pd.__version__)
-
-# Uses for Data analysis, Data science, Machine Learning
-# Microsoft Excel but on Coke.
-
-# Terminal = pip install pandas
-# Terminal = python -m pip install pandas
